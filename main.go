fa31ae4b7957347b21b3213aa026adbeed6d5250
gordun209 <ali-han80@hotmail.com>
2022-11-14 13:35:50 +0300

    doin api

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-
 	_ "github.com/lib/pq"
 )
 
+// simdilk dble urasmayak
 func main() {
 	Main()
 	db := NewDB()
 	defer db.Close()
-	db.InsertUser("alaai", "aydin")
-
-	users := db.getUsers()
-	fmt.Println(users)
-}
-
-func readAPIKeyFromFile() string {
-	file, err := os.Open("apikey")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	return scanner.Text()
 }
--- utils.go
+++ utils.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 const UserLimit = 5
 
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+
+func readAPIKeyFromFile() string {
+	file, err := os.Open("apikey")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return scanner.Text()
 }
